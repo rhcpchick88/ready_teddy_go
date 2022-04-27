b0bf8253376b193359524a4b316121e9d0a5d1f5
samantha <langridge.se@gmail.com>
2022-04-26 22:44:58 -0600

    set up pinia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {createPinia, PiniaVuePlugin} from 'pinia'
 
 Vue.config.productionTip = false
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
 
 new Vue({
+  pinia,
   router,
   render: h => h(App)
 }).$mount('#app')
